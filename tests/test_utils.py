d523aa77907b10ae0c672e4be2015ee2a0dd866e
myback <54638513+myback@users.noreply.github.com>
2023-07-09 23:44:47 +0700

    add second test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,24 @@ from pathlib import Path
 import go_template
 
 
-class TestMethods(unittest.TestCase):
+class TestRender(unittest.TestCase):
     def test_add(self):
         values = {"Count": 12, "Material": "Wool"}
 
         render_data = go_template.render(Path('tests/sample.tmpl'), values).decode()
+
+        with open('tests/test.txt') as f:
+            test_data = f.read()
+
+        self.assertEqual(render_data, test_data)
+
+
+class TestRenderFromValuesFile(unittest.TestCase):
+    def test_add(self):
+        render_data = go_template.render_from_values_file(
+            Path('tests/sample.tmpl'),
+            Path('tests/values.yml')
+        ).decode()
 
         with open('tests/test.txt') as f:
             test_data = f.read()
